f0dcda494e114fd1e58f6893171b3a0c197f49f7
Sean Gillespie <seene3@gmail.com>
2021-12-05 22:11:13 -0500

    started 5 day function

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ var currentTempEl = document.querySelector('#current-temp')
 var currentWindEl = document.querySelector('#current-wind')
 var currentHumidEl = document.querySelector('#current-humid')
 var currentWeatherImg = document.querySelector('#current-weather-img')
+var fiveDayEl = document.querySelector('#five-day')
 
 const getCurrentWeather = async () => {
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInputEl.value.trim()}&units=imperial&appid=beff4d2d3a48b96528fcd84d9d9be94a`
@@ -27,13 +28,32 @@ const getCurrentWeather = async () => {
 }
 
 const getFiveDayWeather = async () => {
+    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInputEl.value.trim()}&units=imperial&appid=beff4d2d3a48b96528fcd84d9d9be94a`
+    const res = await fetch(apiUrl)
+    let data = await res.json()
+    let milliOne = data.list[3].dt * 1000
+    let dateOne = new Date(milliOne)
+    let dateFormatOne = dateOne.toLocaleString("en-US", {month: "short", day: "numeric", year: "numeric"})
+    let milliTwo = data.list[11].dt * 1000
+    let dateTwo = new Date(milliTwo)
+    let dateFormatTwo = dateTwo.toLocaleString("en-US", {month: "short", day: "numeric", year: "numeric"})
+    let milliThree = data.list[19].dt * 1000
+    let dateThree = new Date(milliThree)
+    let dateFormatThree = dateThree.toLocaleString("en-US", {month: "short", day: "numeric", year: "numeric"})
+    let milliFour = data.list[27].dt * 1000
+    let dateFour = new Date(milliFour)
+    let dateFormatFour = dateFour.toLocaleString("en-US", {month: "short", day: "numeric", year: "numeric"})
+    let milliFive = data.list[35].dt * 1000
+    let dateFive = new Date(milliFive)
+    let dateFormatFive = dateFive.toLocaleString("en-US", {month: "short", day: "numeric", year: "numeric"})
+    console.log(data)
     
 }
 
 var getWeather = async (event) => {
     event.preventDefault();
     getCurrentWeather();
-    get5dayWeather();
+    getFiveDayWeather();
 }
 
 searchInputEl.addEventListener('submit', getWeather);
